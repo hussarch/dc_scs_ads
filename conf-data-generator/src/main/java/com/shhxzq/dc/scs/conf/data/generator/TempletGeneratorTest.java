34ecc0f68c5cbb999d7b26c6509c32c00baa2a38
xiaoyi <xiaoyi@shhxzq.com>
2017-07-31 18:16:55 +0800

    refactor

--- conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/TempletGeneratorTest.java
+++ conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/TempletGeneratorTest.java
@@ -1,10 +1,22 @@
 package com.shhxzq.dc.scs.conf.data.generator;
 
 import com.shhxzq.dc.scs.conf.data.generator.website.edu.QuestionAnswerEntity;
+import com.shhxzq.dc.scs.frm.base.page.anotations.ApiDesc;
+import com.shhxzq.dc.scs.frm.base.page.anotations.PageDesc;
+import com.shhxzq.dc.scs.frm.base.page.type.ApiType;
 import com.shhxzq.dc.scs.frm.cdf.TempletGenerator;
 
 public class TempletGeneratorTest {
    
+    @PageDesc(path = { "website", "edu", "qa" }, serviceId = "question_answer", title = "常见问答", params = { "type" })
+    @ApiDesc(serviceId = "qaList", name = "常见问答列表", params = { "type" }, type = ApiType.list)
+    @ApiDesc(serviceId = "qaDetail", name = "常见问答详情", params = { "id" }, type = ApiType.detail)
+    public QuestionAnswerEntity generateQuestionAnswerEntity(QuestionAnswerEntity entity){
+        
+        
+        return null;
+    }
+    
     public static void main(String[] args) {
         String path = "/Users/sailor/git/adapter-data-sys/conf-data/";
         TempletGenerator.writeTemplet(QuestionAnswerEntity.class, path, true);
--- conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/website/edu/QuestionAnswerEntity.java
+++ conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/website/edu/QuestionAnswerEntity.java
@@ -9,90 +9,85 @@ import com.shhxzq.dc.scs.frm.base.page.type.ApiType;
 import com.shhxzq.dc.scs.frm.base.page.type.PageType;
 
 /**
- * @author XiaoYi
- * Created on 2017-07-20 11:00:26
+ * @author XiaoYi Created on 2017-07-20 11:00:26
  */
-@PageDesc(
-        path = {"website", "edu", "qa"},
-        serviceId = "question_answer", title = "常见问答", 
-        params = { "type" })
-    @ApiDesc(serviceId = "qaList", name = "常见问答列表", params = { "type" }, type = ApiType.list)
-    @ApiDesc(serviceId = "qaDetail", name = "常见问答详情", params = { "id" }, type = ApiType.detail)
+@PageDesc(path = { "website", "edu", "qa" }, serviceId = "question_answer", title = "常见问答", params = { "type" })
+@ApiDesc(serviceId = "qaList", name = "常见问答列表", params = { "type" }, type = ApiType.list)
+@ApiDesc(serviceId = "qaDetail", name = "常见问答详情", params = { "id" }, type = ApiType.detail)
 public class QuestionAnswerEntity {
-    
-    @FieldDesc(label = "ID", hidden = true, notShowIn = {PageType.add}, apiType = {ApiType.list, ApiType.detail})
+
+    @FieldDesc(label = "ID", hidden = true, notShowIn = { PageType.add }, apiType = { ApiType.list, ApiType.detail })
     private Integer id;
-    
-    @FieldDesc(label = "问题", search = true, max = 100, apiType = {ApiType.list, ApiType.detail})
+
+    @FieldDesc(label = "问题", search = true, max = 100, apiType = { ApiType.list, ApiType.detail })
     private String question;
-    
-    @FieldDesc(label = "答案", search = true, max = 500, apiType = {ApiType.list, ApiType.detail})
+
+    @FieldDesc(label = "答案", search = true, max = 500, apiType = { ApiType.list, ApiType.detail })
     private String answer;
-    
+
     private String type;
-    
-    @FieldDesc(label = "序号", search = true, showIn = {PageType.table}, hidden = true)
+
+    @FieldDesc(label = "序号", search = true, showIn = { PageType.table }, hidden = true)
     private Integer indexNum;
-    
-    
-    @FieldDesc(label = "创建时间", showIn = {}, apiType = {ApiType.list, ApiType.detail})
+
+    @FieldDesc(label = "创建时间", showIn = {}, apiType = { ApiType.list, ApiType.detail })
     private Date createdAt;
     private Date updatedAt;
-    
+
     public Integer getId() {
         return id;
     }
-    
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public String getQuestion() {
         return question;
     }
-    
+
     public void setQuestion(String question) {
         this.question = question;
     }
-    
+
     public String getAnswer() {
         return answer;
     }
-    
+
     public void setAnswer(String answer) {
         this.answer = answer;
     }
-    
+
     public String getType() {
         return type;
     }
-    
+
     public void setType(String type) {
         this.type = type;
     }
-    
+
     public Integer getIndexNum() {
         return indexNum;
     }
-    
+
     public void setIndexNum(Integer indexNum) {
         this.indexNum = indexNum;
     }
-    
+
     public Date getCreatedAt() {
         return createdAt;
     }
-    
+
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
-    
+
     public Date getUpdatedAt() {
         return updatedAt;
     }
-    
+
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
-    
+
 }
