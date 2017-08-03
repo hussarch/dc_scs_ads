1fac41519101e2257a8c1e965f6c087550b112fc
xiaoyi <xiaoyi@shhxzq.com>
2017-08-03 14:47:33 +0800

    no

--- adapter-data-sys-conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/TempletGeneratorTest.java
+++ adapter-data-sys-conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/TempletGeneratorTest.java
@@ -1,26 +1,6 @@
 package com.shhxzq.dc.scs.conf.data.generator;
 
-import com.shhxzq.dc.scs.conf.data.generator.website.edu.QuestionAnswerEntity;
-import com.shhxzq.dc.scs.frm.cdf.TempletGenerator;
-import com.shhxzq.dc.scs.frm.cdf.templete.anotations.ApiDesc;
-import com.shhxzq.dc.scs.frm.cdf.templete.anotations.CategoryDesc;
-import com.shhxzq.dc.scs.frm.cdf.templete.anotations.PageDesc;
-
 public class TempletGeneratorTest {
    
-    @PageDesc(path = { "website", "edu", "qa" }, name = "常见问答")
-    @ApiDesc(serviceId = "qaList", name = "常见问答列表", params = { "type" })
-    @ApiDesc(serviceId = "qaDetail", name = "常见问答详情", params = { "id" })
-    @CategoryDesc(fieldName = "type", group = {"news:资讯新闻", "edu:教育文档"})
-    public QuestionAnswerEntity generateQuestionAnswerEntity(QuestionAnswerEntity entity){
-        
-        
-        return null;
-    }
-    
-    public static void main(String[] args) {
-        String path = "/Users/sailor/git/adapter-data-sys/conf-data/";
-        TempletGenerator.writeTemplet(QuestionAnswerEntity.class, path, true);
-    }
     
 }
---
+++ adapter-data-sys-conf-data-generator/src/main/java/com/shhxzq/dc/scs/conf/data/generator/demo/DemoTempletGenerator.java
@@ -0,0 +1,71 @@
+package com.shhxzq.dc.scs.conf.data.generator.demo;
+
+import com.shhxzq.dc.da.hxbisys.jpa.entity.UserEntity;
+import com.shhxzq.dc.da.hxbisys.jpa.entity.demo.DocumentEntity;
+import com.shhxzq.dc.da.hxbisys.jpa.entity.demo.QuestionAnswerEntity;
+import com.shhxzq.dc.scs.frm.base.common.type.PageType;
+import com.shhxzq.dc.scs.frm.cdf.TempletGenerator;
+import com.shhxzq.dc.scs.frm.cdf.templete.anotations.ApiDesc;
+import com.shhxzq.dc.scs.frm.cdf.templete.anotations.CategoryDesc;
+import com.shhxzq.dc.scs.frm.cdf.templete.anotations.PageDesc;
+import com.shhxzq.dc.scs.frm.cdf.templete.anotations.PageFieldDesc;
+
+/**
+ * @author XiaoYi
+ * Created on 2017-08-03 14:11:51
+ */
+public class DemoTempletGenerator {
+    
+    @PageDesc(path = { "common", "sys", "user" }, name = "用户", 
+            entityClass = UserEntity.class)
+    @PageFieldDesc(type = PageType.search, showFileds = {"name", "email"})
+    @PageFieldDesc(type = PageType.table, notShowFileds = {"password", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.add, notShowFileds = {"id", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.update, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @PageFieldDesc(type = PageType.view, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    public void defineUserEntity() {
+    }
+    
+    @PageDesc(path = { "common", "sys", "role" }, name = "角色", 
+            entityClass = UserEntity.class)
+    @PageFieldDesc(type = PageType.search, showFileds = {"name", "email"})
+    @PageFieldDesc(type = PageType.table, notShowFileds = {"createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.add, notShowFileds = {"id", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.update, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @PageFieldDesc(type = PageType.view, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    public void defineRoleEntity(){
+    }
+    
+    @PageDesc(path = { "website", "edu", "doc" }, name = "文档", category = "type", entityClass = DocumentEntity.class)
+    @PageFieldDesc(type = PageType.search, showFileds = {"title", "sourceFrom"})
+    @PageFieldDesc(type = PageType.table, notShowFileds = {"content", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.add, notShowFileds = {"id", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.update, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @PageFieldDesc(type = PageType.view, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @ApiDesc(serviceId = "list", name = "文档列表", params = { "type" }, 
+        notShowFileds = { "createdAt", "updatedAt" })
+    @ApiDesc(serviceId = "detail", name = "文档详情", params = { "id" }, 
+        notShowFileds = { "createdAt", "updatedAt" })
+    public void defineDocumentEntity() {
+    }
+    
+    @PageDesc(path = { "website", "edu", "qa" }, name = "问答", entityClass = QuestionAnswerEntity.class)
+    @PageFieldDesc(type = PageType.search, showFileds = {"question"})
+    @PageFieldDesc(type = PageType.table, notShowFileds = {"answer", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.add, notShowFileds = {"id", "createdAt", "updatedAt"})
+    @PageFieldDesc(type = PageType.update, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @PageFieldDesc(type = PageType.view, notShowFileds = {"createdAt", "updatedAt"}, hiddenFileds = "id")
+    @ApiDesc(serviceId = "list", name = "常见问答列表", params = { "type" }, 
+        notShowFileds = { "createdAt", "updatedAt" })
+    @ApiDesc(serviceId = "detail", name = "常见问答详情", params = { "id" }, 
+        notShowFileds = { "createdAt", "updatedAt" })
+    @CategoryDesc(fieldName = "type", group = {"HGT:沪港通", "ZXFG:政策法规", "BASIC:基础知识"})
+    public void defineQaEntity() {
+    }
+    
+    public static void main(String[] args) {
+        String path = "/Users/sailor/git/adapter-data-sys/adapter-data-sys-conf-data/";
+        TempletGenerator.writeTemplet(DemoTempletGenerator.class, "defineQaEntity", path, true);
+
+    }
+}
